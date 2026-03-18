1ada99bef666d2b2959d33621c554c0e4c44d4ac
MAZAVAMANANA Zeppélin Tiavy <zeppelintiavymazavamanana@gmail.com>
2026-03-18 10:04:24 +0300

    Zeppélin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link rel="icon" href="data:," />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     <title>Zeppélin | Dévelopeur web Full Stack</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/main.jsx'])
